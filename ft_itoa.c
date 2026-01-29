715bba52e20805b678b094ff34d4f4a78710ba76
David <110501225+DaviiidSG@users.noreply.github.com>
2026-01-29 15:18:17 +0100

    feat: added functions: ft_split ft_itoa ft_strmapi

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,71 @@
 /*   By: dserrano <dserrano@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 16:32:11 by dserrano          #+#    #+#             */
-/*   Updated: 2026/01/28 21:04:15 by dserrano         ###   ########.fr       */
+/*   Updated: 2026/01/29 01:29:56 by dserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_count_digits(long n)
+{
+	int	count;
+
+	if (!n)
+		return (1);
+	count = 0;
+	while (n)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+static void	ft_fill_str(char *s, int digits, unsigned int is_negative, long n)
+{
+	if (is_negative)
+	{
+		s[digits + 1] = '\0';
+		while (digits)
+		{
+			s[digits] = (n % 10) + '0';
+			n /= 10;
+			digits--;
+		}
+		s[0] = '-';
+	}
+	else
+	{
+		s[digits] = '\0';
+		while (--digits >= 0)
+		{
+			s[digits] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+}
+
 char	*ft_itoa(int n)
 {
-	unsigned int	sign;
-	unsigned int	temp_n;
-	char			*str;
+	long			temp_n;
+	int				digit_count;
+	unsigned int	is_negative;
+	char			*num_str;
 
-	sign = 0;
-	if (n < 0)
+	temp_n = n;
+	digit_count = ft_count_digits(temp_n);
+	is_negative = 0;
+	if (temp_n < 0)
 	{
-		sign = 1;
-		temp_n = -(long)n;
+		is_negative = 1;
+		temp_n = -temp_n;
+		num_str = malloc((digit_count + 2) * sizeof(*num_str));
 	}
+	else
+		num_str = malloc((digit_count + 1) * sizeof(*num_str));
+	if (!num_str)
+		return (NULL);
+	ft_fill_str(num_str, digit_count, is_negative, temp_n);
+	return (num_str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dserrano <dserrano@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:13:36 by dserrano          #+#    #+#             */
-/*   Updated: 2026/01/28 20:13:28 by dserrano         ###   ########.fr       */
+/*   Updated: 2026/01/29 14:25:29 by dserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -43,5 +44,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
