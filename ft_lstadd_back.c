8eb3fed8f71c6510106479898868de27291e6169
David <110501225+DaviiidSG@users.noreply.github.com>
2026-01-31 17:41:21 +0100

    feat: add t_list and functions for handle the list

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dserrano <dserrano@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 16:22:29 by dserrano          #+#    #+#             */
+/*   Updated: 2026/01/31 16:29:13 by dserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	if (!lst || !new)
+		return ;
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+}
